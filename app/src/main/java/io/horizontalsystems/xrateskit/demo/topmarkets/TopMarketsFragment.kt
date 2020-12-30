bea09dd03082961a516a8a6b704c658dff75db61
nk-the-crazy <nurkali@gmail.com>
2020-12-30 14:41:49 +0600

    Add progress and spinner to Markets demo App

--- app/src/main/java/io/horizontalsystems/xrateskit/demo/topmarkets/TopMarketsFragment.kt
+++ app/src/main/java/io/horizontalsystems/xrateskit/demo/topmarkets/TopMarketsFragment.kt
@@ -1,15 +1,17 @@
 package io.horizontalsystems.xrateskit.demo.topmarkets
 
-import android.opengl.Visibility
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.ConcatAdapter
 import io.horizontalsystems.xrateskit.demo.R
+import io.horizontalsystems.xrateskit.entities.TimePeriod
 import kotlinx.android.synthetic.main.fragment_top_markets.*
 
 
@@ -23,11 +25,39 @@ class TopMarketsFragment() : Fragment() {
         return inflater.inflate(R.layout.fragment_top_markets, container, false)
     }
 
+    class SpinnerActivity(var timePeriod: TimePeriod = TimePeriod.HOUR_24) : Activity(), AdapterView.OnItemSelectedListener {
+
+        override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+            timePeriod = if(pos == 0)
+                TimePeriod.HOUR_24
+            else if(pos == 1)
+                TimePeriod.DAY_7
+            else
+                TimePeriod.DAY_30
+        }
+
+        override fun onNothingSelected(parent: AdapterView<*>) {
+            // Another interface callback
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val spAct = SpinnerActivity()
+
+        ArrayAdapter.createFromResource(
+                this.requireContext(),
+                R.array.periods,
+                android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spPeriod.adapter = adapter
+            spPeriod.onItemSelectedListener = spAct
+        }
+
         btnLoadMarkets.setOnClickListener {
-            viewModel.loadTopMarkets()
+            viewModel.loadTopMarkets(spAct.timePeriod)
             rviewInfo2.visibility = View.GONE
             rviewInfo.visibility = View.VISIBLE
         }
@@ -38,7 +68,7 @@ class TopMarketsFragment() : Fragment() {
             rviewInfo2.visibility = View.VISIBLE
         }
         btnLoadDefiMarkets.setOnClickListener {
-            viewModel.loadTopDefiMarkets()
+            viewModel.loadTopDefiMarkets(spAct.timePeriod)
             rviewInfo2.visibility = View.GONE
             rviewInfo.visibility = View.VISIBLE
         }
@@ -59,6 +89,13 @@ class TopMarketsFragment() : Fragment() {
         viewModel.globalMarketInfo.observe(viewLifecycleOwner, Observer {
             globalMarketInfoAdapter.items = it
             globalMarketInfoAdapter.notifyDataSetChanged()
+        })
+
+        viewModel.progressState.observe(viewLifecycleOwner, Observer {
+            if(it)
+                progressBar.visibility = View.VISIBLE
+            else
+                progressBar.visibility = View.GONE
         })
     }
 }
--- app/src/main/java/io/horizontalsystems/xrateskit/demo/topmarkets/TopMarketsViewModel.kt
+++ app/src/main/java/io/horizontalsystems/xrateskit/demo/topmarkets/TopMarketsViewModel.kt
@@ -12,32 +12,39 @@ import java.util.*
 class TopMarketsViewModel(val ratesManager: RatesManager) : ViewModel() {
 
     val topMarkets = MutableLiveData<List<TopMarket>>()
+    val progressState = MutableLiveData<Boolean>()
     val globalMarketInfo = MutableLiveData<List<GlobalMarketInfoItem>>()
 
     private var disposables = CompositeDisposable()
 
-    fun loadTopMarkets() {
-        ratesManager.topList(25, "USD", TimePeriod.DAY_7)
+    fun loadTopMarkets(timePeriod: TimePeriod) {
+        progressState.postValue(true)
+        ratesManager.topList(200, "USD", timePeriod)
             .subscribeOn(Schedulers.io())
             .observeOn(Schedulers.io())
             .subscribe({ topMarketList ->
                            topMarkets.postValue(topMarketList)
+                           progressState.postValue(false)
                        }, {
                             println("Error !!! ${it.message}")
+                            progressState.postValue(false)
                        })
             .let {
                 disposables.add(it)
             }
     }
 
-    fun loadTopDefiMarkets() {
-        ratesManager.topDefiList(25, "USD", TimePeriod.HOUR_24)
+    fun loadTopDefiMarkets(timePeriod: TimePeriod) {
+        progressState.postValue(true)
+        ratesManager.topDefiList(200, "USD", timePeriod)
             .subscribeOn(Schedulers.io())
             .observeOn(Schedulers.io())
             .subscribe({ topMarketList ->
+                           progressState.postValue(false)
                            topMarkets.postValue(topMarketList)
                        }, {
                             println("Error !!! ${it.message}")
+                            progressState.postValue(false)
                        })
             .let {
                 disposables.add(it)
@@ -45,12 +52,15 @@ class TopMarketsViewModel(val ratesManager: RatesManager) : ViewModel() {
     }
 
     fun loadGlobalMarketInfo() {
+        progressState.postValue(true)
         ratesManager.globalMarketInfo("USD")
             .subscribeOn(Schedulers.io())
             .observeOn(Schedulers.io())
             .subscribe({ globalInfo ->
+                           progressState.postValue(false)
                            globalMarketInfo.postValue(GlobalMarketInfoItem.getList(globalInfo))
                        }, {
+                            progressState.postValue(false)
                             println("Error !!! ${it.message}")
                        })
             .let {
