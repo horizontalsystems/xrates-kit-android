b3a10c16b1a56513af0fb095a622018c6b482803
nk-the-crazy <nurkali@gmail.com>
2021-02-23 12:25:25 +0600

    Add additional timeperiods supported by CoinGecko

--- xrateskit/src/main/java/io/horizontalsystems/xrateskit/entities/TimePeriod.kt
+++ xrateskit/src/main/java/io/horizontalsystems/xrateskit/entities/TimePeriod.kt
@@ -6,6 +6,8 @@ enum class TimePeriod(val id: Int, val seconds: Long, val title: String) {
     DAY_START(2, 0, "DayStart"),
     HOUR_24(3, 86400, "24h"),
     DAY_7(4, 604800,"7d"),
+    DAY_14(6, 604800,"14d"),
     DAY_30(5, 2592000, "30d"),
+    DAY_200(5, 2592000, "200d"),
     YEAR_1(6, 31104000, "1y");
 }
