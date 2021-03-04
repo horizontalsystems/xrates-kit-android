825b753d5432bb2da1d4473f7e2dd25e93a2ab69
nk-the-crazy <nurkali@gmail.com>
2021-03-04 18:14:18 +0600

    Add provider fallback for empty chart data response

--- xrateskit/src/main/java/io/horizontalsystems/xrateskit/api/BaseChartInfoProvider.kt
+++ xrateskit/src/main/java/io/horizontalsystems/xrateskit/api/BaseChartInfoProvider.kt
@@ -4,7 +4,10 @@ import io.horizontalsystems.xrateskit.coins.ProviderCoinsManager
 import io.horizontalsystems.xrateskit.core.IChartInfoProvider
 import io.horizontalsystems.xrateskit.entities.ChartInfoKey
 import io.horizontalsystems.xrateskit.entities.ChartPointEntity
+import io.horizontalsystems.xrateskit.entities.ChartType
 import io.reactivex.Single
+import java.math.BigDecimal
+import java.util.*
 
 class BaseChartInfoProvider(
     private val providerCoinsManager: ProviderCoinsManager,
@@ -17,10 +20,28 @@ class BaseChartInfoProvider(
 
         if(!providerIds.isEmpty()){
             providerIds[0]?.let {
-                return cryptoCompareProvider.getChartPoints(chartPointKey)
+                return cryptoCompareProvider.getChartPoints(chartPointKey).flatMap {
+                    if(isChartDataValid(chartPointKey.chartType, it)) Single.just(it)
+                    else coinGeckoProvider.getChartPoints(chartPointKey)
+                }
             }
         }
 
         return coinGeckoProvider.getChartPoints(chartPointKey)
+    }
+
+    private fun isChartDataValid(chartType: ChartType, responseList: List<ChartPointEntity>): Boolean {
+        val lastPoint = responseList.lastOrNull() ?: return false
+
+        val endTimestamp = Date().time / 1000
+        if (endTimestamp - chartType.rangeInterval > lastPoint.timestamp) {
+            return false
+        }
+
+        val values = responseList.filter { it.value.equals(BigDecimal.ZERO) }
+        if(values.size == responseList.size)
+            return false
+
+        return true
     }
 }
