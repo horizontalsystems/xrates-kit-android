3f3b07600ceff86689c18ff8a48d635bc04ac5da
nk-the-crazy <nurkali@gmail.com>
2021-03-12 10:57:06 +0600

    Add provider rate limit checks to ApiManager

--- xrateskit/src/main/java/io/horizontalsystems/xrateskit/api/ApiManager.kt
+++ xrateskit/src/main/java/io/horizontalsystems/xrateskit/api/ApiManager.kt
@@ -9,10 +9,14 @@ import java.nio.charset.StandardCharsets
 import java.util.logging.Logger
 import javax.net.ssl.HttpsURLConnection
 
-class ApiManager {
+class ApiManager(val rateLimit: Int) {
     private val logger = Logger.getLogger("ApiManager")
 
+    private val delayMillis = rateLimit
+    private var lastRequestTime = System.currentTimeMillis()
+
     fun getJson(uri: String, body: String): JsonObject {
+        delayIfNeeded()
 
         logger.info("Doing POST request: $uri")
 
@@ -47,21 +51,36 @@ class ApiManager {
 
     @Throws
     fun getJsonValue(uri: String, requestProperties: Map<String, String> = mapOf()): JsonValue {
+        delayIfNeeded()
+
         logger.info("Fetching $uri")
 
         return URL(uri)
-                .openConnection()
-                .apply {
-                    connectTimeout = 5000
-                    readTimeout = 60000
-                    setRequestProperty("Accept", "application/json")
-                    requestProperties.forEach { (key, value) ->
-                        setRequestProperty(key, value)
-                    }
+            .openConnection()
+            .apply {
+                connectTimeout = 5000
+                readTimeout = 60000
+                setRequestProperty("Accept", "application/json")
+                requestProperties.forEach { (key, value) ->
+                    setRequestProperty(key, value)
                 }
-                .getInputStream()
-                .use {
-                    Json.parse(it.bufferedReader())
-                }
+            }
+            .getInputStream()
+            .use {
+                Json.parse(it.bufferedReader())
+            }
+    }
+
+    @Synchronized private fun delayIfNeeded() {
+        val currentTimeMillis = System.currentTimeMillis()
+        val timePassedFromLastRequest =  currentTimeMillis - lastRequestTime
+        val timeToWait = delayMillis - timePassedFromLastRequest
+        if (timeToWait > 0) {
+            logger.info("Need to delay for $timeToWait millis")
+            Thread.sleep(timeToWait)
+        } else{
+            logger.info("No Need to delay for ($timeToWait millis)")
+        }
+        lastRequestTime = currentTimeMillis
     }
 }
