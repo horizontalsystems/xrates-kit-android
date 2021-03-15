e4839aa39dd7f6485c6a92739e5d57b4cd0c88cd
abdrasulov <abdrasulov@gmail.com>
2021-03-15 18:29:51 +0600

    Refactor ApiManager

--- xrateskit/src/main/java/io/horizontalsystems/xrateskit/api/ApiManager.kt
+++ xrateskit/src/main/java/io/horizontalsystems/xrateskit/api/ApiManager.kt
@@ -3,54 +3,18 @@ package io.horizontalsystems.xrateskit.api
 import com.eclipsesource.json.Json
 import com.eclipsesource.json.JsonObject
 import com.eclipsesource.json.JsonValue
-import java.io.OutputStream
 import java.net.URL
-import java.nio.charset.StandardCharsets
 import java.util.logging.Logger
-import javax.net.ssl.HttpsURLConnection
 
-class ApiManager(val rateLimit: Int) {
+class ApiManager(private val requestDelay: Int) {
     private val logger = Logger.getLogger("ApiManager")
-
-    private val delayMillis = rateLimit
-    private var lastRequestTime = System.currentTimeMillis()
-
-    fun getJson(uri: String, body: String): JsonObject {
-        delayIfNeeded()
-
-        logger.info("Doing POST request: $uri")
-
-        val conn = URL(uri).openConnection() as HttpsURLConnection
-        conn.requestMethod = "POST"
-        return conn
-            .apply {
-                connectTimeout = 30000
-                readTimeout = 60000
-
-                setRequestProperty("Content-Type", "application/json");
-                setRequestProperty("Accept", "application/json")
-                doOutput = true
-                doInput = true
-                requestMethod = "POST"
-
-                val os: OutputStream = outputStream
-                os.write(body.toByteArray(StandardCharsets.UTF_8))
-                os.flush()
-                os.close()
-            }
-            .inputStream
-            .use {
-                Json.parse(it.bufferedReader()).asObject()
-            }
-    }
+    private var lastRequestTime = 0L
 
     @Throws
-    fun getJson(uri: String, requestProperties: Map<String, String> = mapOf()): JsonObject {
-        return getJsonValue(uri, requestProperties).asObject()
-    }
+    fun getJson(uri: String): JsonObject = getJsonValue(uri).asObject()
 
     @Throws
-    fun getJsonValue(uri: String, requestProperties: Map<String, String> = mapOf()): JsonValue {
+    fun getJsonValue(uri: String): JsonValue {
         delayIfNeeded()
 
         logger.info("Fetching $uri")
@@ -61,9 +25,6 @@ class ApiManager(val rateLimit: Int) {
                 connectTimeout = 5000
                 readTimeout = 60000
                 setRequestProperty("Accept", "application/json")
-                requestProperties.forEach { (key, value) ->
-                    setRequestProperty(key, value)
-                }
             }
             .getInputStream()
             .use {
@@ -71,16 +32,18 @@ class ApiManager(val rateLimit: Int) {
             }
     }
 
-    @Synchronized private fun delayIfNeeded() {
-        val currentTimeMillis = System.currentTimeMillis()
-        val timePassedFromLastRequest =  currentTimeMillis - lastRequestTime
-        val timeToWait = delayMillis - timePassedFromLastRequest
+    @Synchronized
+    private fun delayIfNeeded() {
+        if (requestDelay == 0) return
+
+        val currentTime = System.currentTimeMillis()
+        val timePassedFromLastRequest = currentTime - lastRequestTime
+        val timeToWait = requestDelay - timePassedFromLastRequest
         if (timeToWait > 0) {
             logger.info("Need to delay for $timeToWait millis")
             Thread.sleep(timeToWait)
-        } else{
-            logger.info("No Need to delay for ($timeToWait millis)")
         }
-        lastRequestTime = currentTimeMillis
+
+        lastRequestTime = currentTime
     }
 }
