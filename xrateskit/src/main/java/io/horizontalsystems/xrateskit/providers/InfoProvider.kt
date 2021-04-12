81480d7331a8da7ad0fcc595e64dd00e64923c9d
nk-the-crazy <nurkali@gmail.com>
2021-04-12 17:54:14 +0600

    Remove unused resources and CoinPaprika provider classes

--- xrateskit/src/main/java/io/horizontalsystems/xrateskit/providers/InfoProvider.kt
+++ xrateskit/src/main/java/io/horizontalsystems/xrateskit/providers/InfoProvider.kt
@@ -3,7 +3,6 @@ package io.horizontalsystems.xrateskit.providers
 sealed class InfoProvider(val id: String, var baseUrl: String = "", val rateLimit: Int = 0, var accessKey: String? = null) {
 
     class CryptoCompare : InfoProvider("cryptocompare", "https://min-api.cryptocompare.com/")
-    class CoinPaprika : InfoProvider("coinpaprika", "https://api.coinpaprika.com/v1/", 150)
     class CoinGecko : InfoProvider("coingecko", "https://api.coingecko.com/api/v3/", 600)
     class HorSys : InfoProvider("horsys", "https://info.horizontalsystems.xyz/api/v1/", 150)
 
