347149aebe7f77835e2c9fcd54a896c4deef89a2
nk-the-crazy <nurkali@gmail.com>
2021-04-14 16:52:17 +0600

    Fetch  GlobalMarkets data (Points) from HorSys provider

--- xrateskit/src/main/java/io/horizontalsystems/xrateskit/coinmarkets/GlobalMarketInfoManager.kt
+++ xrateskit/src/main/java/io/horizontalsystems/xrateskit/coinmarkets/GlobalMarketInfoManager.kt
@@ -1,49 +1,38 @@
 package io.horizontalsystems.xrateskit.coinmarkets
 
-import io.horizontalsystems.xrateskit.core.IFiatXRatesProvider
 import io.horizontalsystems.xrateskit.core.IGlobalCoinMarketProvider
 import io.horizontalsystems.xrateskit.core.IInfoManager
 import io.horizontalsystems.xrateskit.entities.GlobalCoinMarket
+import io.horizontalsystems.xrateskit.entities.GlobalCoinMarketPointInfo
+import io.horizontalsystems.xrateskit.entities.TimePeriod
 import io.horizontalsystems.xrateskit.storage.Storage
 import io.reactivex.Single
-import java.math.BigDecimal
 
 class GlobalMarketInfoManager(
     private val globalMarketsProvider: IGlobalCoinMarketProvider,
-    private val defiMarketsProvider: IGlobalCoinMarketProvider,
-    private val fiatXRatesProvider: IFiatXRatesProvider,
     private val storage: Storage
 ) : IInfoManager {
 
-    private val BASE_FIAT_CURRENCY = "USD"
+    private val DATA_LIFETIME_SECONDS = 600 // 6 mins
 
-    fun getGlobalMarketInfo(currencyCode: String): Single<GlobalCoinMarket> {
-        return Single.zip(
-            globalMarketsProvider.getGlobalCoinMarketsAsync(currencyCode),
-            defiMarketsProvider.getGlobalCoinMarketsAsync(currencyCode),
-            getFiatXRates(currencyCode),
-            { globalMarket, defiMarket , fiatXRate ->
-                globalMarket.volume24h = globalMarket.volume24h * fiatXRate.toBigDecimal()
-                globalMarket.marketCap = globalMarket.marketCap * fiatXRate.toBigDecimal()
-                globalMarket.defiMarketCap = defiMarket.defiMarketCap * fiatXRate.toBigDecimal()
-                globalMarket.defiMarketCapDiff24h = defiMarket.defiMarketCapDiff24h
-                globalMarket.defiTvl = defiMarket.defiTvl * fiatXRate.toBigDecimal()
-                globalMarket.defiTvlDiff24h = defiMarket.defiTvlDiff24h
-                globalMarket
-            }).map { globalMarketInfo ->
-            storage.saveGlobalMarketInfo(globalMarketInfo)
-                globalMarketInfo
-            }.onErrorReturn {
-                storage.getGlobalMarketInfo(currencyCode)
-            }
-    }
+    fun getGlobalMarketInfo(currencyCode: String, timePeriod: TimePeriod): Single<GlobalCoinMarket> {
 
-    private fun getFiatXRates(currencyCode: String): Single<Double>{
-        if(currencyCode.toUpperCase().contentEquals(BASE_FIAT_CURRENCY)){
-            return Single.just(1.0)
+        val currentTimestamp = System.currentTimeMillis()/1000
+        storage.getGlobalMarketPointInfo(currencyCode, timePeriod)?.let { data ->
+            if((currentTimestamp - data.timestamp ) <= DATA_LIFETIME_SECONDS )
+                return  Single.just(GlobalCoinMarket.calculateData(currencyCode, data.points))
+            else
+                storage.deleteGlobalMarketPointInfo(currencyCode, timePeriod)
         }
 
-        return fiatXRatesProvider.getLatestFiatXRates(BASE_FIAT_CURRENCY, currencyCode)
+        return globalMarketsProvider.getGlobalCoinMarketPointsAsync(currencyCode, timePeriod).map { globalMarketPoints ->
+
+            val pointInfo = GlobalCoinMarketPointInfo(currencyCode = currencyCode, timePeriod = timePeriod, timestamp = currentTimestamp)
+            pointInfo.points.addAll(globalMarketPoints)
+            storage.saveGlobalMarketPointInfo(pointInfo)
+
+            GlobalCoinMarket.calculateData(currencyCode, globalMarketPoints)
+        }
     }
 
     override fun destroy() {
--- xrateskit/src/main/java/io/horizontalsystems/xrateskit/entities/GlobalCoinMarket.kt
+++ xrateskit/src/main/java/io/horizontalsystems/xrateskit/entities/GlobalCoinMarket.kt
@@ -1,20 +1,102 @@
 package io.horizontalsystems.xrateskit.entities
 
-import androidx.room.Entity
+import androidx.room.*
+import androidx.room.ForeignKey.CASCADE
 import java.math.BigDecimal
 
 
-@Entity(primaryKeys = ["currencyCode"])
+//-----------------------------------------------------------
+@Entity(indices = [Index(value = ["currencyCode", "timePeriod"], unique = true)])
+data class GlobalCoinMarketPointInfo(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val currencyCode: String,
+    val timestamp: Long,
+    val timePeriod: TimePeriod
+){
+    @Ignore
+    val points: MutableList<GlobalCoinMarketPoint> = mutableListOf()
+}
+
+//-----------------------------------------------------------
+@Entity(foreignKeys = [
+    ForeignKey(
+        entity = GlobalCoinMarketPointInfo::class,
+        parentColumns = ["id"],
+        childColumns = ["pointInfoId"],
+        onDelete = CASCADE)]
+)
+data class GlobalCoinMarketPoint(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val timestamp: Long,
+    val volume24h: BigDecimal,
+    val marketCap: BigDecimal,
+    val btcDominance: BigDecimal = BigDecimal.ZERO,
+    val defiMarketCap: BigDecimal = BigDecimal.ZERO,
+    val defiTvl: BigDecimal = BigDecimal.ZERO,
+    var pointInfoId: Long = 0
+)
+
+//-----------------------------------------------------------
 data class GlobalCoinMarket(
     val currencyCode: String,
-    var volume24h: BigDecimal,
+    val volume24h: BigDecimal,
     val volume24hDiff24h: BigDecimal,
-    var marketCap: BigDecimal,
+    val marketCap: BigDecimal,
     val marketCapDiff24h: BigDecimal,
-    var btcDominance: BigDecimal = BigDecimal.ZERO,
-    var btcDominanceDiff24h: BigDecimal = BigDecimal.ZERO,
-    var defiMarketCap: BigDecimal = BigDecimal.ZERO,
-    var defiMarketCapDiff24h: BigDecimal = BigDecimal.ZERO,
-    var defiTvl: BigDecimal = BigDecimal.ZERO,
-    var defiTvlDiff24h: BigDecimal = BigDecimal.ZERO
-)
+    val btcDominance: BigDecimal = BigDecimal.ZERO,
+    val btcDominanceDiff24h: BigDecimal = BigDecimal.ZERO,
+    val defiMarketCap: BigDecimal = BigDecimal.ZERO,
+    val defiMarketCapDiff24h: BigDecimal = BigDecimal.ZERO,
+    val defiTvl: BigDecimal = BigDecimal.ZERO,
+    val defiTvlDiff24h: BigDecimal = BigDecimal.ZERO,
+    val globalCoinMarketPoints: List<GlobalCoinMarketPoint>
+){
+
+    companion object {
+        fun calculateData(currencyCode: String, globalMarketPoints: List<GlobalCoinMarketPoint>): GlobalCoinMarket {
+
+            var marketCap = BigDecimal.ZERO
+            var marketCapDiff = BigDecimal.ZERO
+            var defiMarketCap = BigDecimal.ZERO
+            var defiMarketCapDiff = BigDecimal.ZERO
+            var volume24h = BigDecimal.ZERO
+            var volume24hDiff = BigDecimal.ZERO
+            var btcDominance = BigDecimal.ZERO
+            var btcDominanceDiff = BigDecimal.ZERO
+            var tvl = BigDecimal.ZERO
+            var tvlDiff = BigDecimal.ZERO
+
+            if(globalMarketPoints.isNotEmpty()){
+                val startingPoint = globalMarketPoints.last()
+                val endingPoint = globalMarketPoints.first()
+
+                marketCap = endingPoint.marketCap
+                if(startingPoint.marketCap.compareTo(BigDecimal.ZERO) != 0 )
+                    marketCapDiff = ((marketCap - startingPoint.marketCap) * BigDecimal(100))/startingPoint.marketCap
+
+                defiMarketCap = endingPoint.defiMarketCap
+                if(startingPoint.defiMarketCap.compareTo(BigDecimal.ZERO) != 0 )
+                    defiMarketCapDiff = ((defiMarketCap - startingPoint.defiMarketCap) * BigDecimal(100))/startingPoint.defiMarketCap
+
+                volume24h = endingPoint.volume24h
+                if(startingPoint.volume24h.compareTo(BigDecimal.ZERO) != 0 )
+                    volume24hDiff = ((volume24h - startingPoint.volume24h) * BigDecimal(100))/startingPoint.volume24h
+
+                btcDominance = endingPoint.btcDominance
+                if(startingPoint.btcDominance.compareTo(BigDecimal.ZERO) != 0 )
+                    btcDominanceDiff = ((btcDominance - startingPoint.btcDominance) * BigDecimal(100))/startingPoint.btcDominance
+
+                tvl = endingPoint.defiTvl
+                if(startingPoint.defiTvl.compareTo(BigDecimal.ZERO) != 0 )
+                    tvlDiff = ((tvl - startingPoint.defiTvl) * BigDecimal(100))/startingPoint.defiTvl
+
+            }
+
+            return GlobalCoinMarket(currencyCode, volume24h, volume24hDiff, marketCap, marketCapDiff, btcDominance,
+                btcDominanceDiff, defiMarketCap, defiMarketCapDiff, tvl, tvlDiff, globalMarketPoints)
+
+        }
+    }
+}
